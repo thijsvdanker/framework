16bee68ced4359d52b9ac9f03385e2225cb65a13
Daniel Klabbers <dklabbers@hostingxs.nl>
2015-06-29 11:17:14 +0200

    added testcase for unit testing

---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php namespace HynMe\Framework\Testing;
+
+use Illuminate\Foundation\Testing\TestCase as IlluminateTestCase;
+
+class TestCase extends IlluminateTestCase
+{
+    public function setUp()
+    {
+        // run illuminate testCase setup
+        parent::setUp();
+        // register framework service provider and all dependancies
+        $this->app->register('HynMe\Framework\FrameworkServiceProvider');
+    }
+
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../../../../bootstrap/app.php';
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
+}
