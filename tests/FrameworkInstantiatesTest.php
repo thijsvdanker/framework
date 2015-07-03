d47f95edfa5793e8313b70da0629cf649706d0c3
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-03 16:35:31 +0200

    added configuration tester

--- tests/FrameworkInstantiatesTest.php
+++ tests/FrameworkInstantiatesTest.php
@@ -1,11 +1,16 @@
 <?php namespace HynMe\Framework;
 
 use HynMe\Framework\Testing\TestCase;
+use Config;
 
 class FrameworkInstantiatesTest extends TestCase
 {
     public function testProvider()
     {
         $this->assertTrue(in_array(FrameworkServiceProvider::class, $this->app->getLoadedProviders()));
+    }
+    public function testConfigurationProviders()
+    {
+        $this->assertNotEmpty(Config::get('hyn.packages', []), 'no configuration available that lists the hyn packages');
     }
 }
