d578f671dbe7ebd8f39598f3c338d25dc4a9d1a2
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-11-20 23:38:34 +0100

    removed moving into subdirectory

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,19 +4,7 @@
 # v prints every line before executing
 set -ev
 
-cd ${TRAVIS_BUILD_DIR}/laravel
-
-BRANCH_REGEX="^(([[:digit:]]+\.)+[[:digit:]]+)$"
-
-if [[ ${TRAVIS_BRANCH} =~ $BRANCH_REGEX ]]; then
-    echo "composer require hyn/framework:${TRAVIS_BRANCH}"
-    composer require hyn/framework:${TRAVIS_BRANCH}
-else
-    echo "composer require ${TRAVIS_REPO_SLUG}:dev-${TRAVIS_BRANCH}"
-    composer require hyn/framework:dev-${TRAVIS_BRANCH}
-fi
-
 # moves the unit test to the root laravel directory
-cp ./vendor/hyn/framework/phpunit.travis.xml ./phpunit.xml
+cp ./phpunit.travis.xml ./phpunit.xml
 
-phpunit
+phpunit --coverage-text --coverage-clover=coverage.clover
